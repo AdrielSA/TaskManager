3e8a20ba18e87b38ab32c909cdc8a274144c1478
Adriel Simé <Simealburquerque16@outlook.com>
2024-01-14 10:17:49 -0400

    Implementación de reporte

---
+++ src/TaskManager.Web/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using TaskManager.Core.Interfaces.Services;
+
+namespace TaskManager.Web.Controllers
+{
+    public class ReportController(ITaskService taskService, IWebHostEnvironment hostEnvironment) : Controller
+    {
+        private readonly ITaskService _taskService = taskService;
+        private readonly IWebHostEnvironment _hostEnvironment = hostEnvironment; 
+
+        public IActionResult Index()
+        {
+            var path = Path.Combine(_hostEnvironment.WebRootPath, "report", "TaskManagerReport.rdlc");
+            var report = new LocalReport(path);
+
+            var tasks = _taskService.GetTaskList();
+            report.AddDataSource("TaskDataset", tasks);
+
+            var result = report.Execute(RenderType.Pdf);
+
+            return File(result.MainStream, MediaTypeNames.Application.Json);
+        }
+    }
+}
